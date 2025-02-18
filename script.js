93967d8eaf7fca559d0a71d7c0ef28f3ebcc5252
Phos-86 <mauribasdel@gmail.com>
2025-02-18 10:06:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-window.addEventListener('beforeinstallprompt', (event) => {
-  console.log('beforeinstallprompt event fired');
-  event.preventDefault();
-  const installPrompt = event;
-  const installButton = document.getElementById('install-button');
-  installButton.style.display = 'block';
-  installButton.addEventListener('click', () => {
-    console.log('Install button clicked');
-    installPrompt.prompt();
-    installPrompt.userChoice.then((choice) => {
-      if (choice.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-      } else {
-        console.log('User dismissed the install prompt');
-      }
-      installButton.style.display = 'none';
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+      console.log('beforeinstallprompt event fired');
+      event.preventDefault();
+      deferredPrompt = event;
+
+      const installButton = document.getElementById('install-button');
+      installButton.style.display = 'block';
+
+      installButton.addEventListener('click', () => {
+        console.log('Install button clicked');
+        deferredPrompt.prompt();
+
+        deferredPrompt.userChoice.then((choice) => {
+          if (choice.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+          } else {
+            console.log('User dismissed the install prompt');
+          }
+          deferredPrompt = null;
+          installButton.style.display = 'none';
+        });
+      });
     });
-  });
-});
