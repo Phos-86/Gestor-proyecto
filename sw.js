201448d97d853345c6c6a8e9e4b84e630abb642e
Phos-86 <mauribasdel@gmail.com>
2025-02-18 09:49:50 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = gestor;
+const CACHE_NAME = 'gestor'; // Make CACHE_NAME a string
 const urlsToCache = [
   '/Gestor-proyecto/',
   '/Gestor-proyecto/index.html',
@@ -8,13 +8,15 @@ const urlsToCache = [
   '/Gestor-proyecto/hucha.png'
 ];
 
+// Install the service worker and cache the assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(gestor))
+      .then((cache) => cache.addAll(urlsToCache)) // Use urlsToCache instead of gestor
   );
 });
 
+// Fetch event to serve cached assets
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
