fdd3723546901e5ad9872ad0317b255efeaa576c
Phos-86 <mauribasdel@gmail.com>
2025-02-20 16:27:56 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -785,8 +785,8 @@ function reloadGoals() {
     goals.forEach(goal => addGoalToDOM(goal));
 }
 
-const successSound = new Audio('/sounds/success.wav');
-const failureSound = new Audio('/sounds/failure.mp3');
+const successSound = new Audio('/Gestor-proyecto/sounds/success.wav');
+const failureSound = new Audio('/Gestor-proyecto/sounds/failure.mp3');
 
 function playSuccessSound() {
     successSound.play().catch(error => {
